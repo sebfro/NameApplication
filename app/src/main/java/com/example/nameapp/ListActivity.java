f747c9ec67d1f007be928a0dadf5a33d017c0c73
sebfro <sebastian17pepp@gmail.com>
2017-01-19 13:43:46 +0100

    First upload

--- app/src/main/java/com/example/nameapp/ListActivity.java
+++ app/src/main/java/com/example/nameapp/ListActivity.java
@@ -3,6 +3,7 @@ package com.example.nameapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -10,9 +11,6 @@ import java.util.HashMap;
 
 public class ListActivity extends AppCompatActivity {
 
-    private ListView lv;
-    ArrayAdapter<String> adapter;
-    ArrayList<HashMap<String,String>> nameList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +20,9 @@ public class ListActivity extends AppCompatActivity {
         //NameList
         String names[] = {"Sebastian","fedme","andreas"};
 
-        lv = (ListView)findViewById(R.id.list_view);
+        ListView lv = (ListView)findViewById(R.id.list_view);
 
-        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.people_name, names);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.people_name, names);
+        lv.setAdapter(adapter);
     }
 }
