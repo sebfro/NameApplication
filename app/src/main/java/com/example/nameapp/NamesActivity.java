c30b1b73ca52d06271cf8dc7261547e0668e1ca5
sebfro <sebastian17pepp@gmail.com>
2017-01-20 13:05:36 +0100

    First upload

--- app/src/main/java/com/example/nameapp/NamesActivity.java
+++ app/src/main/java/com/example/nameapp/NamesActivity.java
@@ -1,9 +1,14 @@
 package com.example.nameapp;
 
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NamesActivity extends AppCompatActivity {
     String[] names = {"Sebastian", "Daniel", "Fedme"};
@@ -17,5 +22,11 @@ public class NamesActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.nameList);
         listView.setAdapter(adapter);
+    }
+
+    public void nameClicked(View v){
+        TextView temp = v.get
+        TextView tV = (TextView)findViewById(R.id.label);
+        Toast.makeText(getApplicationContext(), tV.getText(),Toast.LENGTH_SHORT).show();
     }
 }
